20c0fc176209876d61444cc4d70e073e86218b08
Kevin Keo <keokev01@students.zhaw.ch>
2024-05-07 10:35:02 +0200

    update

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask, request
 from flask_cors import CORS
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import cross_val_score
 import pickle
 import os
 
@@ -20,25 +16,32 @@ def predict():
     bfs_number = int(request.args['bfs_number'])
     area = float(request.args['area'])
     rooms = float(request.args['rooms'])
-    
+    model_type = request.args.get('model_type', 'randomforest')  # Standardmäßig wird Random Forest verwendet
+
+    # Laden der BFS-Daten
     df_bfs_data = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bfs_municipality_and_tax_data.csv'),
-                            sep=',', encoding='utf-8')
-    df_bfs_data['tax_income'] = df_bfs_data['tax_income'].str.replace("'", "")
-    df = df_bfs_data[df_bfs_data['bfs_number']==bfs_number]
+                              sep=',', encoding='utf-8')
+    df_bfs_data['tax_income'] = df_bfs_data['tax_income'].str.replace("'", "").astype(int)
+    df = df_bfs_data[df_bfs_data['bfs_number'] == bfs_number]
 
-    randomforest_model = RandomForestRegressor()
-    model_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "randomforest_regression.pkl")
-    with open(model_filename, 'rb') as f:
-        randomforest_model = pickle.load(f)
+    if model_type == 'polynomial':
+        # Laden des polynomialen Modells
+        model_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "polynomial_model.pkl")
+        with open(model_filename, 'rb') as f:
+            model = pickle.load(f)
+    else:
+        # Laden des Random Forest Modells
+        model_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "randomforest_regression.pkl")
+        with open(model_filename, 'rb') as f:
+            model = pickle.load(f)
 
-    # ['rooms' 'area' 'pop' 'pop_dens' 'frg_pct' 'emp' 'tax_income' 'm2_per_rooms']
-    prediction = randomforest_model.predict([[rooms, area, df['pop'].iloc[0], df['pop_dens'].iloc[0], df['frg_pct'].iloc[0], df['emp'].iloc[0], df['tax_income'].iloc[0], area/rooms]])
-    return str(round(prediction[0],2))
+    # Vorhersage
+    prediction = model.predict([[rooms, area, df['pop'].iloc[0], df['pop_dens'].iloc[0], df['frg_pct'].iloc[0], df['emp'].iloc[0], df['tax_income'].iloc[0], area/rooms]])
+    return str(round(prediction[0], 2))
 
 @app.route("/")
 def hello_world():
-
-    print(request.args)
-
     return "<p>Hello, World!</p>"
-    
+
+if __name__ == "__main__":
+    app.run(debug=True)
